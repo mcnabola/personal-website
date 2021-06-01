aef8450c3ac7d76a8233005a62936fb9da10c865
mcnabola <17226163@studentmail.ul.ie>
2021-06-02 00:38:43 +0100

    Add route api to allow viewing of MD blogs

---
+++ src/pages/{mdx.slug}.js
@@ -0,0 +1,32 @@
+import { graphql } from "gatsby";
+import { MDXRenderer } from "gatsby-plugin-mdx";
+import React from "react";
+import { Box } from "theme-ui";
+
+export default function PostPage ({data}) {
+    const {
+        body,
+        fontmatter: {title},
+    } = data.mdx;
+    return (<>
+        <Box as="h1" variant="styles.h1" fontSize="4xl">
+            {title}
+        </Box>
+        <MDXRenderer>{body}</MDXRenderer>
+    </>
+    );
+}
+
+export const query = graphql`
+query POST_BY_SLUG($slug: String) {
+  mdx(slug: { eq: $slug }) {
+    id
+    slug
+    body
+    frontmatter {
+      date
+      title
+    }
+  }
+}
+`;
