f1478b29c95dda9000f95d0bcf901cf904383905
mcnabola <17226163@studentmail.ul.ie>
2021-06-02 02:24:50 +0100

    Added relative images for blogs

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,26 @@ module.exports = {
   siteMetadata,
   plugins: [
     `gatsby-plugin-theme-ui`,
-    `gatsby-plugin-mdx`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-mdx`,
+        options: {
+          gatsbyRemarkPlugins: [
+            {
+              resolve: 'gatsby-remark-images',
+              options: {
+                maxWidth: 640,
+              },
+            },
+          ],
+        },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+      path: `${__dirname}/content/`,
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
